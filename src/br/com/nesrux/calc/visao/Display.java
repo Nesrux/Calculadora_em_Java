911bfc51c3fc26f7546b6efd38615df994dad286
Nesrux <joaomarcosdevs@gmail.com>
2023-02-03 17:35:54 -0300

    pegando os dados do input do botao

--- src/br/com/nesrux/calc/visao/Display.java
+++ src/br/com/nesrux/calc/visao/Display.java
@@ -7,13 +7,15 @@ import java.awt.Font;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import br.com.nesrux.calc.modelo.Memoria;
+
 @SuppressWarnings("serial")
 public class Display extends JPanel {
 	private final JLabel label;
 
 	public Display() {
 		setBackground(new Color(46, 49, 50));
-		label = new JLabel("123.456");
+		label = new JLabel(Memoria.getInstancia().getTextoAtual());
 		label.setForeground(Color.WHITE);
 		label.setFont(new Font("courier", Font.PLAIN, 30));
 		
--- src/br/com/nesrux/calc/visao/Teclado.java
+++ src/br/com/nesrux/calc/visao/Teclado.java
@@ -3,11 +3,14 @@ package br.com.nesrux.calc.visao;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
-public class Teclado extends JPanel {
+public class Teclado extends JPanel implements ActionListener {
 	private final Color CINZA_ESCURO = new Color(68, 68, 68);
 	private final Color CINZA_CLARO = new Color(99, 99, 99);
 	private final Color LARANJA = new Color(242, 163, 60);
@@ -57,7 +60,16 @@ public class Teclado extends JPanel {
 		c.gridx = x;
 		c.gridy = y;
 		Botao botao = new Botao(texto, cor);
+		botao.addActionListener(this);
 		add(botao, c);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() instanceof JButton) {
+			JButton botao = (JButton) e.getSource();
+			System.out.println(botao.getText());
+		}
 	}
 
 }
