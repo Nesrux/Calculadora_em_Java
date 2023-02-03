134b47d574bb2a186d1ceebbe3477f8a9368c034
Nesrux <joaomarcosdevs@gmail.com>
2023-02-03 14:38:40 -0300

    configurando o display da calc

--- src/br/com/nesrux/calc/visao/Display.java
+++ src/br/com/nesrux/calc/visao/Display.java
@@ -1,13 +1,26 @@
 package br.com.nesrux.calc.visao;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Display extends JPanel {
+	private final JLabel label;
+
 	public Display() {
-		setBackground(Color.green);
+		setBackground(new Color(46, 49, 50));
+
+		label = new JLabel("123.456");
+		label.setForeground(Color.white);
+		label.setFont(new Font("courier", Font.PLAIN, 30));
+
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+
+		add(label);
 	}
 
 }
