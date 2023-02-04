cb5e0b0e5c987084dfa2c0bd66c3d71d63bdaebb
Nesrux <joaomarcosdevs@gmail.com>
2023-02-04 16:24:48 -0300

    Adicionando enuns e comando ZERAR

--- src/br/com/nesrux/calc/modelo/Memoria.java
+++ src/br/com/nesrux/calc/modelo/Memoria.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Memoria {
+
+	private enum TipoComando {
+		ZERAR, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA
+	};
+
 	private static final Memoria instancia = new Memoria();
 
 	private final List<MemoriaObservador> observadores = new ArrayList<>();
@@ -26,8 +31,32 @@ public class Memoria {
 	}
 
 	public void processarComando(String valor) {
-		textoAtual += valor;
-		observadores.forEach(o -> o.valorAlterado(textoAtual));
+		TipoComando tipoComando = detectarTipoComando(valor);
+		System.out.println(tipoComando);
+
+		if ("AC".equals(valor)) {
+			textoAtual = "";
+		} else {
+			textoAtual += valor;
+			observadores.forEach(o -> o.valorAlterado(textoAtual));
+		}
+	}
+
+	private TipoComando detectarTipoComando(String valor) {
+		if (textoAtual.isEmpty() && valor == "0") {
+			return null;
+		}
+		try {
+			Integer.parseInt(valor);
+			return TipoComando.NUMERO;
+		} catch (NumberFormatException e) {
+			//Quando nao for número cai aqui
+			if("AC".equals(valor)) {
+				return TipoComando.ZERAR;
+			}
+		}
+
+		return null;
 	}
 
 }
