f3933b8a7b35d0f396408fd430cdb19d388db059
Nesrux <joaomarcosdevs@gmail.com>
2023-02-04 18:39:24 -0300

    Adicionando a logica das operacoes matematicas

--- src/br/com/nesrux/calc/modelo/Memoria.java
+++ src/br/com/nesrux/calc/modelo/Memoria.java
@@ -47,10 +47,39 @@ public class Memoria {
 		} else if (tipoComando == TipoComando.NUMERO || tipoComando == TipoComando.VIRGULA) {
 			textoAtual = substituir ? texto : textoAtual + texto;
 			substituir = false;
+		} else {
+			substituir = true;
+			textoAtual = obterResultadoOperacao();
+
+			textoBuffer = textoAtual;
+
+			ultimaOperacao = tipoComando;
 		}
-		
-			observadores.forEach(o -> o.valorAlterado(textoAtual));
-		
+
+		observadores.forEach(o -> o.valorAlterado(textoAtual));
+	}
+
+	private String obterResultadoOperacao() {
+		if (ultimaOperacao == null) {
+			return textoAtual;
+		}
+		double numeroBuffer = Double.parseDouble(textoBuffer.replace(",", "."));
+
+		double numeroAtual = Double.parseDouble(textoAtual.replace(",", "."));
+
+		double resultado = 0;
+
+		if (ultimaOperacao == TipoComando.SOMA) {
+			resultado = numeroBuffer + numeroAtual;
+		}else if (ultimaOperacao == TipoComando.SUB) {
+			resultado = numeroBuffer - numeroAtual;
+		}else if (ultimaOperacao == TipoComando.MULT) {
+			resultado = numeroBuffer * numeroAtual;
+		}else if (ultimaOperacao == TipoComando.DIV) {
+			resultado = numeroBuffer / numeroAtual;
+		}
+
+		return textoAtual;
 	}
 
 	private TipoComando detectarTipoComando(String texto) {
